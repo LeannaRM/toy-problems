6ccaeeac46d080a476b5fcd4f88d1e18ba6638b3
Leanna McDonald <leanna.rose.mcdonald@gmail.com>
2017-01-25 09:59:34 -0600

    Start of day 3 Twelve days of Christmas problem

--- 003-twelvedays.js
+++ 003-twelvedays.js
@@ -1,5 +1,5 @@
 var ordinals = ["first", "second", "third", "fourth", "fifth", "sixth", 
-				"seventh", "eighth", "ninth", "tenth", "eleventh", "twelvth"];
+				"seventh", "eighth", "ninth", "tenth", "eleventh", "twelfth"];
 var objects = ["Partridge in a Pear Tree", "Turtle Doves", "French Hens",
 				"Calling Birds", "Golden Rings", "Geese a Laying", 
 				"Swans a Swimming", "Maids a Milking", "Ladies Dancing", 
@@ -8,6 +8,9 @@ var objects = ["Partridge in a Pear Tree", "Turtle Doves", "French Hens",
 
 for(var i = 0; i<12; i++){
 	var day = i + 1;
-	console.log("On the " + ordinals[i] + " day of Christmas\nmy true love sent to me:\n" 
-				+ day + " " + objects[i]);
+	console.log("On the " + ordinals[i] + " day of Christmas\nmy true love sent to me:");
+	for (var j = day; j>1; j--){
+		console.log(j + " " + objects[j - 1]);
+	}
+	console.log("\n");
 }
