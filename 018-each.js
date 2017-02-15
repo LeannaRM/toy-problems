a36d24543fbc919a3832d44b51bae916f75f5bd8
Leanna McDonald <leanna.rose.mcdonald@gmail.com>
2017-02-15 09:23:02 -0600

    toy problem for 018

---
+++ 018-each.js
@@ -0,0 +1,12 @@
+function each(array,functionname) {
+	for (i=0;i<array.length;i++) {
+		functionname(array[i]);
+	}
+}
+
+var names = ["Sumeet", "Beth", "Dan", "Mindy"]
+
+each(names, function(n){
+	console.log("Isn't " + n + " just great?");
+
+});
